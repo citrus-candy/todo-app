aa9f1877dd5b7439e3bb6db466eb486fe31d513e
kyuuri11013 <kyuuri11013@gmail.com>
2022-04-07 16:04:28 +0900

    change: ormconfig option

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -2,7 +2,9 @@ import { ConnectionOptions } from 'typeorm';
 
 const ormconfig: ConnectionOptions = {
   type: 'postgres',
-  url: process.env.DATABASE_URL,
+  url:
+    process.env.DATABASE_URL ||
+    'postgres://kyuuri1101:@localhost:5432/postgres',
   synchronize: true,
   logging: false,
   entities: ['dist/entities/**/*.entity.js'],
@@ -12,7 +14,10 @@ const ormconfig: ConnectionOptions = {
     migrationsDir: 'src/migrations',
     subscribersDir: 'src/subscribers',
   },
-  ssl: { rejectUnauthorized: false },
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : false,
 };
 
 export default ormconfig;
